fe01e177e2b714e899edecdf2457f9c45d28e994
Rosa Mística <rosamistiica@gmail.com>
2026-03-01 22:31:35 -0300

    Update and rename 1004 to 1004.js

---
+++ 1004.js
@@ -0,0 +1,9 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+let a = parseInt(lines[0]);
+let b = parseInt(lines[1]);
+
+let produto = a * b;
+
+console.log("PROD = " + produto);
