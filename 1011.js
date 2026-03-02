a9b84cb4606f65ec25001fbb470e00b6fe232fa3
Rosa Mística <rosamistiica@gmail.com>
2026-03-01 23:39:14 -0300

    Update 1011.js

--- 1011.js
+++ 1011.js
@@ -1,5 +1,9 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+let raio = parseFloat(lines[0]);
+
 let pi =  3.14159;
-let raio = parseFloat(lines.shift());
 
 let volume = (4/3) * pi * Math.pow(raio,3);
 
