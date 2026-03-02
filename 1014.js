e3e7b3d1570f0932b9e883d3d5f6ef3b0b1bb3c4
Rosa Mística <rosamistiica@gmail.com>
2026-03-01 23:40:23 -0300

    Refactor input handling for distance and fuel

--- 1014.js
+++ 1014.js
@@ -1,5 +1,8 @@
-let distanciatotal = parseInt(lines.shift());
-let totalcombustivel = parseFloat(lines.shift());
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+let distanciatotal = parseInt(lines[0]);
+let totalcombustivel = parseFloat(lines[1]);
 
 let consumoMedio = distanciatotal / totalcombustivel;
 
